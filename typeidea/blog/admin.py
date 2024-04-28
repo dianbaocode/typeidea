65b36a240da3041ba1f8ad827ae2f54e6d9c00d6
dianbaocode <dian313@126.com>
2024-04-28 16:12:05 +0800

    准备开发用户界面前（7

--- typeidea/blog/admin.py
+++ typeidea/blog/admin.py
@@ -1,15 +1,40 @@
 from django.contrib import admin
+from django.contrib.admin import AdminSite
 from django.urls import reverse
 from django.utils.html import format_html
 
+from .adminforms import PostAdminForm
 from .models import Category, Tag, Post
+
+from typeidea.custom_site import custom_site
+from typeidea.base_admin import BaseOwnerAdmin
 
 
 # Register your models here.
-@admin.register(Category)
-class CategoryAdmin(admin.ModelAdmin):
+
+
+# class CustomSite(AdminSite):
+#     site_header = 'TX'
+#     site_title = 'TX管理后台'
+#     index_title = '首页'
+#
+#
+# custom_site = CustomSite()
+# custom_site = CustomSite(name='cus_admin')
+
+
+class PostInline(admin.TabularInline):
+    fields = ('title', 'desc',)
+    extra = 1
+    model = Post
+
+
+@admin.register(Category, site=custom_site)
+class CategoryAdmin(BaseOwnerAdmin):
     list_display = ('name', 'status', 'is_nav', 'created_time', 'post_count',)
     fields = ('name', 'status', 'is_nav')
+
+    # inlines = [PostInline, ]
 
     def post_count(self, obj):
         return obj.post_set.count()
@@ -21,8 +46,8 @@ class CategoryAdmin(admin.ModelAdmin):
         super().save_model(request, obj, form, change)
 
 
-@admin.register(Tag)
-class TagAdmin(admin.ModelAdmin):
+@admin.register(Tag, site=custom_site)
+class TagAdmin(BaseOwnerAdmin):
     list_display = ('name', 'status', 'created_time')
     fields = ('name', 'status',)
 
@@ -46,19 +71,39 @@ class CategoryOwnerFilter(admin.SimpleListFilter):
         return queryset
 
 
-@admin.register(Post)
-class PostAdmin(admin.ModelAdmin):
+@admin.register(Post, site=custom_site)
+class PostAdmin(BaseOwnerAdmin):
+    form = PostAdminForm
     list_display = ('title', 'category', 'status', 'created_time', 'operator',)
-    fields = (
-        ('category', 'title'),
-        'desc',
-        'status',
-        'content',
-        'tag',
+    # fields = (
+    #     ('category', 'title'),
+    #     'desc',
+    #     'status',
+    #     'content',
+    #     'tag',
+    # )
+    fieldsets = (
+        ('基础配置', {
+            'description': '基础配置描述',
+            'fields': (
+                ('title', 'category'), 'status',
+            ),
+        }),
+        ('内容', {
+            'fields': (
+                'desc', 'content',
+            ),
+        }),
+        ('额外信息', {
+            # 'classes': ('collapse', ),
+            'fields': ('tag',),
+        }),
     )
+    filter_horizontal = ('tag',)
+    # filter_vertical = ('tag', )
     list_display_links = []
     # list_filter = ('category', )
-    list_filter = (CategoryOwnerFilter, )
+    list_filter = (CategoryOwnerFilter,)
 
     search_fields = ('title', 'category__name',)
     actions_on_top = True
@@ -67,10 +112,16 @@ class PostAdmin(admin.ModelAdmin):
 
     def operator(self, obj):
         return format_html('<a href="{}">编辑</a>',
-                           reverse('admin:blog_post_change', args=(obj.pk,)))
+                           reverse('cus_admin:blog_post_change', args=(obj.pk,)))
 
     operator.short_description = '操作'
 
     def save_model(self, request, obj, form, change):
         obj.owner = request.user
         super().save_model(request, obj, form, change)
+
+    class Media:
+        css = {
+            'all': ('https://cdn.bootcss.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css',),
+        }
+        js = ('https://cdn.bootcss.com/bootstrap/4.0.0-beta.2/js/bootstrap.bundle.js',)
